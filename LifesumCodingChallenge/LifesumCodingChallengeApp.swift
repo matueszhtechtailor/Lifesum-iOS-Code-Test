2ff99d7805de40e0b532439d0a117e0235a8b1ab
Mateusz Holubowski <mateusz.holubowski@gmail.com>
2022-05-28 15:13:00 +0200

    Initial Commit

---
+++ LifesumCodingChallenge/LifesumCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifesumCodingChallengeApp.swift
+//  LifesumCodingChallenge
+//
+//  Created by Mateusz Holubowski on 28/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct LifesumCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
